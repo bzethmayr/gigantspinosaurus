05e9b83d72da4c40886052e8d615d4c092eda381
Ben Zethmayr <gribnitt@gmail.com>
2026-04-30 15:19:42 -0500

    style

--- src/test/java/net/bzethmayr/gigantspinosaurus/usage/vk/VkContextTest.java
+++ src/test/java/net/bzethmayr/gigantspinosaurus/usage/vk/VkContextTest.java
@@ -8,13 +8,14 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.lwjgl.system.MemoryStack.stackPush;
 
 class VkContextTest {
 
     @Test
     void allLayerNames_givenStack_returnsLayerNames() {
         final List<String> layerNames;
-        try (final MemoryStack stack = MemoryStack.stackPush()) {
+        try (final MemoryStack stack = stackPush()) {
             layerNames = VkContext.allLayerNames(stack);
         }
 
